f71aa3c564f832656dd0e39d75a6a3e84ece62b7
Abhinav <abhinav.022000@gmail.com>
2021-04-09 15:09:48 +0530

    map added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import Button from '@material-ui/core/Button'
+//import L from 'leaflet'
 
 function App() {
+  // const myIcon = L.icon({
+  //   iconUrl: 'dirrections_bus',
+  //   iconSize: [64,64]
+  // });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <MapContainer style={{ height: '100vh' }} center={[51.505, -0.09]} zoom={20} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          <h3>This is the lead of nowhere! Comeonnnn!</h3>
+          <Button variant="contained" color="primary">
+            Read about me :))
+          </Button>
+        </Popup>
+      </Marker>
+      <Marker position={[28.5355161, 77.3910265]}>
+        <Popup>
+          OMG!
+        </Popup>
+      </Marker>
+    </MapContainer>
+  )
 }
 
 export default App;
