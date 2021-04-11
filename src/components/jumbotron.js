c4189919e37dba587108b82446ef182b89d22490
Abhinav <abhinav.022000@gmail.com>
2021-04-11 10:30:26 +0530

    headline fix

--- src/components/jumbotron.js
+++ src/components/jumbotron.js
@@ -5,7 +5,7 @@ const jumbotron = (props) => {
   return (
     <div>
       <Jumbotron>
-        <h1 className="display-3">L_Map!</h1>
+        <h1 className="display-3">GDSC Lead Map &lt;&gt;</h1>
         <p className="lead">Google Developer Student Club Leads are passionate leaders at their university who are dedicated to helping their peers learn and connect. These Leads may be pursuing various undergraduate or graduate university degrees but have a good foundational knowledge of software development concepts.</p>
         <hr className="my-2" />
         <p>Google collaborates with Leads and supports them as they start and grow their on-campus community.</p>
