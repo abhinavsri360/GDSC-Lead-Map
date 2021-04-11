e2aef63e7564fdc7009f3fe8f749eae315d514f2
Abhinav Srivastava <abhinav.022000@gmail.com>
2021-04-11 15:41:17 +0530

    remove one obj

--- src/components/json/worldData.js
+++ src/components/json/worldData.js
@@ -17,21 +17,6 @@ const data = [
         "linkedin": "https://www.linkedin.com/in/abhinavsri360/",
         "twitter": "https://twitter.com/abhinavsri360",
         "portfolio": "http://abhinavsri360.netlify.app/"
-    },
-    {
-        "longitude": 70.3653,
-        "latitude": 22.5192,
-        "batch": 2019,
-        "photo": abhinavsri360,
-        "country": "Europe",
-        "skills": ["AI/ML"],
-        "name": "Harsh Jain",
-        "bio": "I write something colorful on something very dark.",
-        "url": "https://dsc.community.dev/jaypee-institute-of-information-technologec-128/",
-        "chapterName": "Jaypee Institute of Information Technology, Sec - 128",
-        "linkedin": "https://www.linkedin.com/in/abhinavsri360/",
-        "twitter": "https://twitter.com/abhinavsri360",
-        "portfolio": "http://abhinavsri360.netlify.app/"
     }
 ]
 
