88bef25277b1c91a2b5663299bc257864a0cd4b2
Aravind Nair <22199259+aravindvnair99@users.noreply.github.com>
2021-04-11 18:08:32 +0530

    Fix link of GithubCorner and open in new tab
    Signed-off-by: Aravind Nair <22199259+aravindvnair99@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
-import React from 'react'
-import Grid from '@material-ui/core/Grid'
-import GithubCorner from 'react-github-corner'
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import GithubCorner from "react-github-corner";
 
 //Components
-import Map from './components/map'
-import Jumbotron from './components/jumbotron'
+import Map from "./components/map";
+import Jumbotron from "./components/jumbotron";
 
 function App() {
-  return (
-    <>
-      <div className='landing-page d-flex justify-content-center '>
-        <GithubCorner href="https://github.com/abhinavsri360/L_Map" size={100} />
-        <Grid style={{ paddingTop: '1%' }} alignItems='center' justify='center' container spacing={0}>
-          <Grid item xs={11}>
-            <Jumbotron />
-          </Grid>
-          <Grid item xs={11}>
-            <Map />
-          </Grid>
-        </Grid>
-      </div>
-    </>
-  )
+	return (
+		<>
+			<div className="landing-page d-flex justify-content-center ">
+				<GithubCorner
+					href="https://github.com/abhinavsri360/GDSC-Lead-Map"
+					target="_blank"
+					size={100}
+				/>
+				<Grid
+					style={{paddingTop: "1%"}}
+					alignItems="center"
+					justify="center"
+					container
+					spacing={0}
+				>
+					<Grid item xs={11}>
+						<Jumbotron />
+					</Grid>
+					<Grid item xs={11}>
+						<Map />
+					</Grid>
+				</Grid>
+			</div>
+		</>
+	);
 }
 
 export default App;
